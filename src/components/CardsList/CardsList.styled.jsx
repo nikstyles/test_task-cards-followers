596db46cc686dfe912bc6e63e1d18ba4286b01fc
Nikita <nicstyle22@gmail.com>
2023-03-02 02:37:49 +0300

    fix

--- src/components/CardsList/CardsList.styled.jsx
+++ src/components/CardsList/CardsList.styled.jsx
@@ -3,12 +3,15 @@ import styled from 'styled-components';
 export const List = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  justify-content: flex-start;
+  justify-content: center;
   margin: 0;
   padding: 0;
   gap: 30px;
   margin-top: 100px;
   margin-bottom: 100px;
+  @media (min-width: 768px) {
+    justify-content: flex-start;
+  }
   @media (min-width: 1280px) {
     justify-content: center;
   }
